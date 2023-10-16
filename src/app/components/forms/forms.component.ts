e41add384dd9547aaa4516e71a90c95c468beab5
AquilesDias <kilesax@gmail.com>
2023-10-16 09:42:15 -0300

    feat : add filter units opened

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,7 @@
 import { SmartUnitService } from '../../service/smart-unit.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from 'src/app/type/location.interface';
 
 @Component({
   selector: 'app-forms',
@@ -9,21 +10,34 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class FormsComponent implements OnInit {
 
-  results = [];
+  results: Location[] = [];
+  resultsFiltered: Location[] = [];
   formGroup!:FormGroup;
+ 
 
-  constructor( private formBuilder : FormBuilder, private service: SmartUnitService){}
+  constructor( 
+        private formBuilder : FormBuilder, 
+        private service: SmartUnitService
+      ){}
 
   ngOnInit(): void {
-    this.service.findUnits().subscribe(data => console.log(data));
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false
+      showClosed: true
+    });
+
+    this.service.findUnits().subscribe(data => {
+      this.results = data.locations;
+      this.resultsFiltered = data.locations;
     });
   }
 
   onSubmit(): void {
-    console.log(this.formGroup.value);
+    if(!this.formGroup.value.showClosed){
+      this.resultsFiltered = this.results.filter(location => location.opened === true);
+    } else {
+      this.resultsFiltered = this.results;
+    }
   }
   onClean(): void {
     this.formGroup.reset();
--- src/app/type/smartfit-units-response.interface.ts
+++ src/app/type/smartfit-units-response.interface.ts
@@ -1,3 +1,5 @@
+import { Location } from "./location.interface";
+
 export interface SmartfitUnit {
     current_country_id : number,
     locations          : Location[]
